eebb487ff578d9ab5a3064566541f88f8eb0950e
Aniket Artani <aniketartani25@gmail.com>
2022-04-11 00:04:18 +0530

    Add files via upload

---
+++ build/precache-manifest.272a4a36959d9949cbbe20ae10bc93cc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "237235041c1c813411459ffac8baca85",
+    "url": "/react-admin-dashboard/index.html"
+  },
+  {
+    "revision": "2be8b10dd91400cb5c33",
+    "url": "/react-admin-dashboard/static/css/main.c722d9b5.chunk.css"
+  },
+  {
+    "revision": "d4a33c564be5ead8f1cf",
+    "url": "/react-admin-dashboard/static/js/2.d4d59059.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-admin-dashboard/static/js/2.d4d59059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80a30b2f0200d9c3ade1",
+    "url": "/react-admin-dashboard/static/js/3.222577b8.chunk.js"
+  },
+  {
+    "revision": "2ee4a329ded82e8e12215f331bb46922",
+    "url": "/react-admin-dashboard/static/js/3.222577b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d2a46daceb6686aed3",
+    "url": "/react-admin-dashboard/static/js/4.6436a125.chunk.js"
+  },
+  {
+    "revision": "2be8b10dd91400cb5c33",
+    "url": "/react-admin-dashboard/static/js/main.ada7c453.chunk.js"
+  },
+  {
+    "revision": "a92892c4bca65ad59a51",
+    "url": "/react-admin-dashboard/static/js/runtime-main.3ed62eb4.js"
+  }
+]);
